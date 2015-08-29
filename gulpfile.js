5aca031d653a12f00d12101fdd18c40aeebd010f
Mazhar Koding <mazhar@koding.com>
2015-08-29 06:23:03 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var elixir = require('laravel-elixir'),
+    gulp   = require('gulp'),
+    jade   = require('gulp-jade'),
+    util   = require('gulp-util');
+
+elixir(function(mix) {
+
+  gulp.task('jade', function() {
+    gulp.src('resources/assets/jade/**/*.jade')
+      .pipe(jade({
+        pretty: !util.env.production
+      }))
+      .pipe(gulp.dest('public/'));
+  });
+
+  mix
+    .sass()
+    .coffee()
+    .task('jade', 'resources/assets/jade/**/*.jade');
+});
